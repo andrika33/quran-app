169a39594d29f35476cd48528d5e6bc16411d487
Andrika <andrika@ihsansolusi.co.id>
2023-02-18 22:56:38 +0700

    fix conflict

--- src/utils/Services.js
+++ src/utils/Services.js
@@ -12,23 +12,14 @@ import { baseUrlSurah, baseUrlJadwal } from "../config/config.js";
 // const exampleDelete = ()=> Delete(baseUrlSurah, `example/${id}`)
 
 // GET
-<<<<<<< HEAD
 const getSurah = () => Get(baseUrlSurah, "surah");
-const getJadwal = (id_kota, tahun, bulan, tanggal) =>
-  Get(baseUrlJadwal, `sholat/jadwal/${id_kota}/${tahun}/${bulan}/${tanggal}`);
-
-const API = {
-  getSurah,
-  getJadwal,
-};
-=======
-const getSurah = ()=> Get(baseUrlSurah, 'surah')
-const getSurahById = (id)=> Get(baseUrlSurah, `surah/${id}`)
+const getJadwal = (id_kota, tahun, bulan, tanggal) => Get(baseUrlJadwal, `sholat/jadwal/${id_kota}/${tahun}/${bulan}/${tanggal}`);
+const getSurahById = (idSurah)=> Get(baseUrlSurah, `surah/${idSurah}`)
 
 const API = {
     getSurah,
-    getSurahById
+    getSurahById,
+    getJadwal
 }
->>>>>>> b3274cd956987aafbceae09f01756d75b85143ae
 
 export default API;
